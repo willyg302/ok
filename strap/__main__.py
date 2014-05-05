5af14b7b7733949e306f8c62b08bc5525c1aa64e
William Gaul <willyg302@gmail.com>
2014-05-05 13:07:30 -1000

    Working on grabbing logic

--- strap/__main__.py
+++ strap/__main__.py
@@ -1,3 +1,3 @@
-import sys
+import strap
 
-print sys.argv
+strap.main()
--- strap/strap.py
+++ strap/strap.py
@@ -1,6 +1,8 @@
 import os
+import sys
 import platform
 import importlib
+import re
 
 from subprocess import call
 
@@ -47,13 +49,57 @@ def install_requirements():
 	call_virtual('pip freeze > {}'.format(REQUIREMENTS))
 
 
-def main():
+def install(root):
 	print('== Installing jarvis Kernel ==')
-	os.chdir(DIRNAME)  # Make sure we work in directory next to current file
-	check_dependencies()
-	check_env()
-	install_requirements()
+	# @TODO: Error-check that this exists
+	os.chdir(root)  # Make sure we work in appropriate directory
+
+	print os.getcwd()
+	# check_dependencies()
+	# check_env()
+	# install_requirements()
 	print('== Kernel Installation Complete! ==')
+
+
+
+
+# Clones a project from a remote URI [source] into [dest]
+def clone(source, dest):
+	github = '(gh|github)\:(?://)?'
+	url = 'git://github.com/{}.git'.format(re.sub(github, '', source)) if re.search(github, source) else source
+	print('Cloning git repo "{}" to "{}"...'.format(url, dest))
+	call('git clone {} {}'.format(url, dest), shell=True)
+	install(dest)
+
+def copy(source, dest):
+	print 'copy'
+
+
+def done(err):
+	print('=== Strapping complete! {} ==='.format(err if err else 'No error!'))
+
+
+def run(source, dest=None, callback=None):
+	print('=== Fetching project ===')
+	if not callback:
+		callback = done
+	if re.search('(?:https?|git(hub)?|gh)(?:://|@)?', source):
+		clone(source, dest if dest else os.getcwd())
+	else:
+		if dest:
+			copy(source, dest)
+		else:
+			install(source)
+	callback(None)
+
+
+def main():
+	if len(sys.argv) < 2:
+		print('Usage: strap <uri-or-path-to-repo> [path-to-output-dir]')
+		return
+	source = sys.argv[1]
+	dest = sys.argv[2] if len(sys.argv) > 2 else ''
+	run(source, dest)
 
 
 if __name__ == '__main__':
