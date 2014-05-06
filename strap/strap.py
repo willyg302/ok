26c08fd7537317568ff72e27379ee29aeb6f1f97
William Gaul <willyg302@gmail.com>
2014-05-05 19:03:05 -1000

    This might work

--- strap/strap.py
+++ strap/strap.py
@@ -1,17 +1,32 @@
+from __future__ import print_function
+
 import os
 import sys
 import platform
 import importlib
 import re
+import shutil
+import json
+import contextlib
 
 from subprocess import call
 
 
-DIRNAME = os.path.abspath(os.path.dirname(__file__))
-ENV = 'env'
-REQUIREMENTS = 'requirements.txt'
+STRAP_FILE = 'strap.json'
+ENV = None
 
 script_dir = '\\Scripts\\' if platform.system() == 'Windows' else '/bin/'
+
+
+@contextlib.contextmanager
+def directory(path):
+	'''Context manager for changing the current working directory'''
+	prev_cwd = os.getcwd()
+	os.chdir(path)
+	try:
+		yield
+	finally:
+		os.chdir(prev_cwd)
 
 
 # Checks to see if [package] is installed, and if not calls [command] to install it
@@ -37,59 +52,104 @@ def check_dependencies():
 
 # Check if our virtual environment is already created, and create if not
 def check_env():
+	if not ENV:
+		return
 	print('== 2. Checking virtual environment ==')
 	if not os.path.isdir(ENV):
 		print('Creating virtual environment at {}...'.format(os.path.basename(ENV)))
 		call('virtualenv {}'.format(ENV), shell=True)
 
 # Install all necessary requirements to the virtual environment
-def install_requirements():
+def install_requirements(requirements):
 	print('== 3. Installing requirements ==')
-	call_virtual('pip install sockjs-tornado')
-	call_virtual('pip freeze > {}'.format(REQUIREMENTS))
+	for requirement in requirements:
+		call_virtual(requirement)
 
 
 def install(root):
-	print('== Installing jarvis Kernel ==')
-	# @TODO: Error-check that this exists
-	os.chdir(root)  # Make sure we work in appropriate directory
+	if not os.path.isdir(root):
+		raise Exception('"{}" is not a valid directory!'.format(root))
+	with directory(root):
+		if not os.path.isfile(STRAP_FILE):
+			raise Exception('Missing configuration file "{}"!'.format(STRAP_FILE))
+		with open(STRAP_FILE) as f:
+			config = json.load(f)
+		print('=== Installing {} ==='.format(config['project']))
+		check_dependencies()
+		for task in config['tasks']:
+			task_root = task['root'] if 'root' in task else '.'
+			global ENV
+			ENV = task['virtualenv'] if 'virtualenv' in task else None
+			if not os.path.isdir(task_root):
+				raise Exception('"{}" is not a valid directory!'.format(task_root))
+			with directory(task_root):
+				check_env()
+				install_requirements(task['requirements'])
+				if 'freeze' in task:
+					call_virtual('pip freeze > {}'.format(task['freeze']))
+	print('=== {} Installation Complete! ==='.format(config['project']))
 
-	print os.getcwd()
-	# check_dependencies()
-	# check_env()
-	# install_requirements()
-	print('== Kernel Installation Complete! ==')
 
+def query_yes_no(query, default='yes'):
+	valid = {
+		'yes': True,
+		'y': True,
+		'no': False,
+		'n': False
+	}
+	if default not in ['yes', 'no', None]:
+		default = None
+	while True:
+		choice = raw_input('{} [{}/{}]: '.format(query, 'Y' if default == 'yes' else 'y', 'N' if default == 'no' else 'n')).lower()
+		if default and not choice:
+			return valid[default]
+		elif choice in valid:
+			return valid[choice]
+		else:
+			print('Please respond with "yes" or "no" (or "y" or "n").')
 
-
+# Deletes [dir] if it exists and the user approves
+def verify_write_directory(dir):
+	if os.path.isdir(dir):
+		if not query_yes_no('The directory "{}" already exists! Overwrite?'.format(dir)):
+			raise Exception('Operation aborted by user.')
+		shutil.rmtree(dir)
 
 # Clones a project from a remote URI [source] into [dest]
 def clone(source, dest):
+	verify_write_directory(dest)
 	github = '(gh|github)\:(?://)?'
 	url = 'git://github.com/{}.git'.format(re.sub(github, '', source)) if re.search(github, source) else source
 	print('Cloning git repo "{}" to "{}"...'.format(url, dest))
 	call('git clone {} {}'.format(url, dest), shell=True)
 	install(dest)
 
+# Copies a project from a local directory [source] to [dest]
 def copy(source, dest):
-	print 'copy'
-
+	verify_write_directory(dest)
+	shutil.copytree(source, dest)
+	install(dest)
 
 def done(err):
-	print('=== Strapping complete! {} ==='.format(err if err else 'No error!'))
-
+	if err:
+		print(err, file=sys.stderr)
+	print('=== Strapping complete! {} ==='.format('There were errors.' if err else 'No error!'))
 
 def run(source, dest=None, callback=None):
 	print('=== Fetching project ===')
 	if not callback:
 		callback = done
-	if re.search('(?:https?|git(hub)?|gh)(?:://|@)?', source):
-		clone(source, dest if dest else os.getcwd())
-	else:
-		if dest:
-			copy(source, dest)
+	try:
+		if re.search('(?:https?|git(hub)?|gh)(?:://|@)?', source):
+			clone(source, dest if dest else os.getcwd())
 		else:
-			install(source)
+			if dest:
+				copy(source, dest)
+			else:
+				install(source)
+	except Exception, e:
+		callback(e)
+		return
 	callback(None)
 
 
