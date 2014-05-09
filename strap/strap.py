de332590e301fdfc6dffe44fd94079deb818436e
William Gaul <willyg302@gmail.com>
2014-05-09 11:28:17 -1000

    Using docopt now

--- strap/strap.py
+++ strap/strap.py
@@ -1,3 +1,17 @@
+'''
+usage: strap
+       strap (--version)
+       strap <command> [<args>...]
+
+options:
+  -h, --help
+
+Subcommands include:
+  init  Initialize a project
+  run   Run tasks on a project
+
+See 'strap help <command>' for more information on a specific command.
+  '''
 from __future__ import print_function
 
 import os
@@ -7,10 +21,10 @@ import importlib
 import re
 import shutil
 import contextlib
-import argparse
 import imp
 
 from subprocess import call
+from lib.docopt import docopt
 
 
 STRAP_FILE = 'strapme.py'
@@ -22,6 +36,8 @@ script_dir = '\\Scripts\\' if platform.system() == 'Windows' else '/bin/'
 @contextlib.contextmanager
 def directory(path):
 	'''Context manager for changing the current working directory'''
+	if not os.path.isdir(path):
+		raise Exception('"{}" is not a valid directory!'.format(path))
 	prev_cwd = os.getcwd()
 	os.chdir(path)
 	try:
@@ -59,9 +75,10 @@ def bootstrap_package(package, command):
 # Check for and installs dependencies
 def check_dependencies():
 	log(3, 'Checking dependencies')
-	bootstrap_package('setuptools', 'python setup/ez_setup.py')
-	bootstrap_package('pip', 'python setup/get-pip.py')
-	bootstrap_package('virtualenv', 'pip install virtualenv')
+	with directory(os.path.dirname(os.path.abspath(__file__))):  # This has to be done relative to strap.py
+		bootstrap_package('setuptools', 'python lib/ez_setup.py')
+		bootstrap_package('pip', 'python lib/get-pip.py')
+		bootstrap_package('virtualenv', 'pip install virtualenv')
 
 # Check if our virtual environment is already created, and create if not
 def check_env():
@@ -78,8 +95,6 @@ def run_task(tasklist, taskname):
 	task_root = normalize_path(task['root']) if 'root' in task else '.'
 	global ENV
 	ENV = normalize_path(task['virtualenv']) if 'virtualenv' in task else None
-	if not os.path.isdir(task_root):
-		raise Exception('"{}" is not a valid directory!'.format(task_root))
 	with directory(task_root):
 		check_env()
 		for item in task['run']:
@@ -94,8 +109,6 @@ def run_task(tasklist, taskname):
 			shell('pip freeze > {}'.format(task['freeze']))
 
 def _run(dir, tasks):
-	if not os.path.isdir(dir):
-		raise Exception('"{}" is not a valid directory!'.format(dir))
 	with directory(dir):
 		if not os.path.isfile(STRAP_FILE):
 			raise Exception('Missing configuration file "{}"!'.format(STRAP_FILE))
@@ -155,11 +168,10 @@ def _init(source, dest):
 	log(5, 'Fetching project')
 	if re.search('(?:https?|git(hub)?|gh)(?:://|@)?', source):
 		clone(source, dest if dest else os.getcwd())
+	elif dest:
+		copy(source, dest)
 	else:
-		if dest:
-			copy(source, dest)
-		else:
-			_run(source, ['install'])
+		_run(source, ['install'])
 
 
 def done(err):
@@ -168,6 +180,14 @@ def done(err):
 	log(5, 'Strapping complete! {}'.format('There were errors.' if err else 'No error!'))
 
 def run(dir, tasks, callback=done):
+	'''usage: strap run -h
+       strap run [-v] [--dir=PATH] <task>...
+
+options:
+  -h, --help
+  -v, --verbose
+  -d PATH --dir PATH
+  '''
 	try:
 		_run(dir, tasks)
 	except Exception, e:
@@ -176,6 +196,14 @@ def run(dir, tasks, callback=done):
 	callback(None)
 
 def init(source, dest, callback=done):
+	'''usage: strap init -h
+       strap init [-v] [--dest=PATH] <source>
+
+options:
+  -h, --help
+  -v, --verbose
+  -d PATH --dest PATH
+  '''
 	try:
 		_init(source, dest)
 	except Exception, e:
@@ -185,26 +213,26 @@ def init(source, dest, callback=done):
 
 
 def main():
-	parser = argparse.ArgumentParser(prog='strap')
-	subparsers = parser.add_subparsers()
+	args = docopt(__doc__, version='strap version 0.1.0', options_first=True)
+	command = args['<command>']
+	argv = [command] + args['<args>']
+	if not command:
+		command = 'run'
+		argv = ['run', 'default']
 
-	run_parser = subparsers.add_parser('run')
-	run_parser.add_argument('tasks', nargs='+')
-	run_parser.add_argument('--dir', default=os.getcwd())
-
-	init_parser = subparsers.add_parser('init')
-	init_parser.add_argument('src')
-	init_parser.add_argument('--dest')
-
-	if len(sys.argv) == 1:
-		args = parser.parse_args(['run', 'default'])
+	if command == 'help':
+		if args['<args>'] and args['<args>'][0] in 'init run'.split():
+			print(docopt(globals()[args['<args>'][0]].__doc__, argv=['-h']))
+		else:
+			print(__doc__.strip('\n'))
+	elif command == 'init':
+		subargs = docopt(init.__doc__, argv=argv)
+		init(subargs['<source>'], subargs['--dest'])
+	elif command == 'run':
+		subargs = docopt(run.__doc__, argv=argv)
+		run(subargs['--dir'] or os.getcwd(), subargs['<task>'])
 	else:
-		args = parser.parse_args()
-
-	if hasattr(args, 'tasks'):
-		run(args.dir, args.tasks)
-	else:
-		init(args.src, args.dest)
+		print('Invalid command "{}". See "strap help".'.format(command))
 
 
 if __name__ == '__main__':
